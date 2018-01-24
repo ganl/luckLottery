2052b9e5406fcdaa2778cca9819ed7e094b41b7c
ganl <769323213@qq.com>
2018-01-24 16:32:00 +0800

    tiny img

---
+++ img/items.org/rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path=./
+j=0
+
+for i in `ls $path | grep '^image[0-9]*\.png$'`
+do
+	$((j = ${j}+1))
+	echo $j
+	echo $i
+	name=`echo $i | sed 's/^image[0-9]*/image'$j'/g'`      
+	echo $name
+       	mv $i  $name
+done
+
