e787193145bb1cd17e42e75ec4419675d80907b0
ganl <769323213@qq.com>
2021-01-18 21:44:43 +0800

    根据user.json定义加载人员头像

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ var winW = $(window).width();
 var winH = $(window).height();
 
 //运动旋转音效
-var runingmic=document.getElementById("runingmic");
-runingmic.volume=0.5;
+var runingmic = document.getElementById("runingmic");
+runingmic.volume = 0.5;
 
 //中奖音效
-var pausemic=document.getElementById("pausemic");
-pausemic.volume=1.0;
+var pausemic = document.getElementById("pausemic");
+pausemic.volume = 1.0;
 
 var levelType = -1;
 var luckNum = 0;
@@ -19,89 +19,82 @@ var cNum = 0;
 
 var timer = null;
 
-// 假定总共参与抽奖的人数，会从localStorage中更新
-var totalperson = 190;
-
 container.css({
     width: winW + 'px',
     height: winH + 'px'
 });
 
-/*-----------------------------初始化抽奖数据---------------------------------------*/
-
-var initdatas = localStorage.getItem('initdatas') ? JSON.parse(localStorage.getItem('initdatas')) : [];
-if (!initdatas.length) {
-    for (var i = 1; i <= totalperson; i++) { // store index
-        if (i < 10) {
-            initdatas.push('00' + i);
-        } else if (i < 100) {
-            initdatas.push('0' + i);
-        } else {
-            initdatas.push(i + '');
-        }
-    }
-
-    localStorage.setItem('initdatas', JSON.stringify(initdatas))
-}
-
-
-/*-----------------------------------开始抽奖-----------------------------------------*/
-var one = $('#one');
-var two = $('#two');
-var three = $('#three');
-var ball = $('#ball');
-var out = $('div.out');
 var selectnumber = $('div.select-number');
 var totalClass = [];
 var allClass = [];
 var firstType = "";
 
-var levelTypeTitle = ['特等奖', '一等奖', '二等奖', '三等奖', '幸运奖'];
-var LuckNumTitle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/*-----------------------------初始化抽奖数据---------------------------------------*/
 
-//已中奖的号码
-var isWinningeds = localStorage.getItem('isWinningeds') ? JSON.parse(localStorage.getItem('isWinningeds')) : [];
-var isWinningedsJson = localStorage.getItem('isWinningedsJson') ? JSON.parse(localStorage.getItem('isWinningedsJson')) : [];
+// 假定总共参与抽奖的人数，会从localStorage中更新
+var totalPerson = 340;
 
-// 根据参数获得随机数
-function getNumberForRadom(max, min) {
-    return parseInt(Math.random() * ((max - min) + 1));
+var prizes = JSON.parse(localStorage.getItem('prizes'));
+if (!prizes) {
+    alert('请先设置好奖品');
 }
 
-//获得中奖号码
+var users = JSON.parse(localStorage.getItem('users'));
+if (!users || users.length === 0) {
+    alert('请先设置好参与人员');
+} else {
+    totalPerson = users.length;
+}
+
+var players = JSON.parse(localStorage.getItem('players')) || [];
+if (!players.length) {
+    players = JSON.parse(localStorage.getItem('users'));
+    if (players) {
+        localStorage.setItem('players', JSON.stringify(players));
+    }
+}
+
+var levelTypeTitle = prizes.map(function (item) {
+    return item.name
+}) || ['特等奖', '一等奖', '二等奖', '三等奖'].reverse();
+var LuckNumTitle = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//已中奖的工号
+var isWinningeds = JSON.parse(localStorage.getItem('isWinningeds')) || [];
+var isWinningedsJson = JSON.parse(localStorage.getItem('isWinningedsJson')) || [];
+
+/*-----------------------------------开始抽奖-----------------------------------------*/
+
+// 根据参数获得随机数 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// getRandomIntInclusive
+function getNumberForRadom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //含最大值，含最小值 
+}
+
+//获得中奖人员
 function winningFn(type) {
     console.log(JSON.stringify(isWinningeds.sort(function (x, y) {
         return x - y
     })));
-    var oneth = getNumberForRadom(9, 0); //个位
-    var twoth = getNumberForRadom(9, 0); //十位
 
-    var threeth = parseInt(oneth + twoth * 10); //十位
-    var baith = totalperson.toString().substr(1, 2); //切取后面两位数
+    if (isWinningeds.length >= totalPerson || players.length < 1) {
+        alert('剩余抽奖人数不足！！！ 请点击右下角垃圾桶，清除已抽奖数据，再重新开始');
+        return false;
+    }
 
     var isrepeat = false; //是否中过奖
 
     // 中奖的索引号
-    var winningeIndex = 0;
-
-    //中奖号码索引
-    if (threeth >= baith) {
-        winningeIndex = getNumberForRadom(0, 0) * 100 + threeth
-    } else {
-        winningeIndex = getNumberForRadom(1, 0) * 100 + threeth
-    }
-
-    if(isWinningeds.length >= totalperson){
-        // alert('剩余抽奖人数不足！！！ 请清空浏览器缓存，再重新开始');
-        // return false;
-    }
-
-    // 判断是否是重复中奖号码
+    var winningeIndex = getNumberForRadom(0, players.length - 1);
+    console.log('winningeIndex' + winningeIndex);
+    // 判断是否是重复中奖
     if (isWinningeds && isWinningeds.length) {
         for (var i = 0; i < isWinningeds.length; i++) {
-            if (isWinningeds[i] === winningeIndex) {
+            if (isWinningeds[i] === players[winningeIndex].gonghao) {
                 isrepeat = true;
-                console.log(winningeIndex + '--- repeat ------------- re-random --------');
+                console.log(winningeIndex + '--- repeat ----- ' + players[winningeIndex].gonghao + ' -------- re-random --------');
             }
         }
     }
@@ -109,17 +102,20 @@ function winningFn(type) {
     if (isrepeat) {
         return winningFn(type);
     } else {
-        // 保存已经中奖的号码
-        isWinningeds.push(winningeIndex);
+        // 保存已经中奖的工号
+        console.log(players[winningeIndex], players)
+        isWinningeds.push(parseInt(players[winningeIndex].gonghao.trim()));
         localStorage.setItem('isWinningeds', JSON.stringify(isWinningeds))
         isWinningedsJson.push({
-            type: type,
-            name: (userList[winningeIndex] && userList[winningeIndex].name) || 'unknown',
-            value: initdatas[winningeIndex]
+            level: type,
+            prize: levelTypeTitle[type],
+            name: (players[winningeIndex] && players[winningeIndex].name) || 'unknown',
+            value: players[winningeIndex].gonghao,
+            avatar: './img/avatar/' + players[winningeIndex].avatar || ('image' + ('0000' + players[winningeIndex].gonghao.trim()).slice(-3) + '.png')
         });
         localStorage.setItem('isWinningedsJson', JSON.stringify(isWinningedsJson))
-        console.log('winningFn ' + winningeIndex, initdatas[winningeIndex]);
-        return winningeIndex
+        console.log('winningFn ' + winningeIndex, players[winningeIndex]);
+        return players.splice(winningeIndex, 1)[0]
     }
 }
 
@@ -140,7 +136,7 @@ function showHaveHTML(type) {
         var newArr = [];
         if (isWinningedsJson && isWinningedsJson.length) {
             for (var i = 0; i < isWinningedsJson.length; i++) {
-                if (isWinningedsJson[i].type == type) {
+                if (isWinningedsJson[i].level == type) {
                     newArr.push(isWinningedsJson[i])
                 }
             }
@@ -149,24 +145,32 @@ function showHaveHTML(type) {
             for (var i = 0; i < newArr.length; i++) {
                 html += '<div class="select-number list" style="right:10px;top:0px;transform: scale(0.6,0.6);margin:0 auto;position:relative">\
 						<div class="block">\
-                            <img src="./img/items/image' + ( '0000' + (userList[(parseInt(newArr[i].value) % totalperson)].index) ).slice(-3) + '.png" class="img-circle">\
-                            <span>' + userList[(parseInt(newArr[i].value) % totalperson)].name + '</span>\
+                            <img src="' + newArr[i].avatar + '" class="img-circle">\
+                            <span>' + newArr[i].name + '</span>\
 						</div>\
 					</div>';
             }
         }
+
         $('#luck-list').append(html);
-        // container.append(html);
     }
 }
 
 function setPrizeLevel(level) {
-    $('#sphere').click();
+    clearInterval(timer)
+    setRotationY(0);
+
+    $('#'+['helix', 'grid'][getNumberForRadom(0, 1)]).click()
     levelType = level;
     cNum = 0; // 清零
     var $levelType = $('#levelType');
 
     $levelType.html(levelTypeTitle[levelType]);
+
+    setTimeout(function() {
+        $('#sphere').click();
+        rotationBall(3000);
+    }, 3000);
 }
 
 function setLuckNum(num) {
@@ -180,6 +184,11 @@ function setLuckNum(num) {
 // 开始抽奖
 function setPrize() {
 
+    if (isWinningeds.length >= totalPerson || players.length < 1) {
+        alert('剩余抽奖人数不足！！！ 请点击右下角垃圾桶，清除已抽奖数据，再重新开始');
+        return false;
+    }
+
     var type = -1;
     if (levelType == -1) {
         alert('请选择奖项类型');
@@ -188,10 +197,12 @@ function setPrize() {
         type = levelType;
     }
 
-    if(luckNum == 0){
+    if (luckNum == 0) {
         alert('请选择中奖人数');
         return;
     }
+    // 停止自动旋转
+    clearInterval(timer);
 
     //显示已经中奖的号码
     console.log(JSON.stringify(isWinningedsJson));
@@ -203,57 +214,59 @@ function setPrize() {
 
     var personArrObj = [];
 
-    if(running) { // if running, then stop it. and get the lucky man
+    if (running) { // if running, then stop it. and get the lucky man
 
         $('button.btn-success').attr('disabled', true);
 
-        for(var i = 0; i < luckNum; i++) {
+        for (var i = 0; i < luckNum; i++) {
             /*------------------------------抽奖开始------------------------------------*/
 
-            // 中奖号码
-            var winninge = initdatas[winningFn(type)];
-            console.log('setPrize ' + winninge);
-
+            // 中奖对象
+            var winner = winningFn(type);
+            console.log('get Prize ' + levelTypeTitle[type], winner);
+            if (winner === false) {
+                break;
+            }
             // 塞入中奖html
-            var zhongjiangclass = randomString(6);
+            var winnerclass = randomString(6);
 
             // 界面上显示中奖号码
-            getHTML(winninge, zhongjiangclass);
+            getHTML(winner, winnerclass);
 
-            // 打印中奖号码和索引
-            console.log('中奖的索引(array下标):' + winninge);
+            // 打印中奖号码
+            console.log('中奖的工号:' + winner.gonghao);
 
             /*------------------------------抽奖结束------------------------------------*/
 
             // 存入数组
             allClass.push({
-                classname: zhongjiangclass,
+                classname: winnerclass,
                 type: type
             });
             totalClass.push({
-                classname: zhongjiangclass,
+                classname: winnerclass,
                 type: type
             });
 
             // 生成抽奖右移动画
-            var keyframes = "@keyframes " + zhongjiangclass + "{\
+            var keyframes = "@keyframes " + winnerclass + "{\
 				0% {opacity:1;right:50%;top:50%;transform: scale(1,1);margin:-100px -240px 0 0;}\
 				50%{opacity:0.1;}\
 				100% {opacity:1;right:10px;top:0px;transform: scale(0.6,0.6);margin:0;position:relative;}\
 			}";
             $('#styles').append(keyframes);
 
+            var imgUrl = './img/avatar/' + winner.avatar || ('image' + ('0000' + user.gonghao.trim()).slice(-3) + '.png');
+            var userName = winner.name;
+            var gonghao = winner.gonghao;
 
-            // var imgUrl = userList[(parseInt(winninge) % totalperson)].avatar;
-            var imgUrl = './img/items/image' + ( '0000' + (userList[(winninge % totalperson)].index) ).slice(-3) + '.png';
-            var userName = userList[(parseInt(winninge) % totalperson)].name
-
-            personArrObj.push({imgurl: imgUrl, username: userName, zhongjiangclass: zhongjiangclass});
-
+            personArrObj.push({ imgurl: imgUrl, username: userName, gonghao: gonghao, winnerclass: winnerclass });
         }
 
+        localStorage.setItem('players', JSON.stringify(players));
+
         runingmic.pause();
-        
+
         showLuckAnimate(levelTypeTitle[levelType], personArrObj);
 
         // if(luckNum == cNum) { // 一次抽奖的人数已经达到，允许下次抽奖
@@ -276,7 +289,6 @@ function setPrize() {
 
     // 转起来
     rotateBall();
-
 }
 
 // 显示获奖标题
@@ -295,18 +307,18 @@ function showLuckAnimate(showLevel, personObj) {
     pausemic.currentTime = 0;
     pausemic.play();
 
-    var zhongjiangclasses = [];
+    var winnerclasses = [];
 
-    if(luckNum > 1){
+    if (luckNum > 1) {
         container.append('<div class="animate-bg"><div class="lottery-animate-bg"><div id="multi-lottery"></div>');
-        for(var i = 0; i < luckNum; i++) {
+        for (var i = 0; i < luckNum; i++) {
             $('#multi-lottery').append('<div class="lotteryuserhead2"><img src="' + personObj[i].imgurl + '"/><span class="user-name">' + personObj[i].username + '</span></div></div>');
-            zhongjiangclasses.push(personObj[i].zhongjiangclass);
+            winnerclasses.push(personObj[i].winnerclass);
         }
 
-        $('.lottery-animate-bg').append('<div class="clearfix"></div><div class="confirm-btn"><button class="btn btn-lg btn-success fl" onclick="confirmPrize(\''+ zhongjiangclasses.join(',') +'\')"><span id="confirmWin">确定</span></button></div>');
+        $('.lottery-animate-bg').append('<div class="clearfix"></div><div class="confirm-btn"><button class="btn btn-lg btn-success fl" onclick="confirmPrize(\'' + winnerclasses.join(',') + '\')"><span id="confirmWin">确定</span></button></div>');
     } else {
-        container.append('<div class="animate-bg"><div class="light"></div><div class="lottery-animate-bg"><div class="lotteryuserhead"><img src="' + personObj[0].imgurl + '"/></div><div class="level">恭喜<span class="user-name">' + personObj[0].username + '</span>获得<p class="awards-name">' + showLevel + '</p></div><div class="confirm-btn"><button class="btn btn-lg btn-success fl" onclick="confirmPrize(\''+ personObj[0].zhongjiangclass +'\')"><span id="confirmWin">确定</span></button></div></div>');
+        container.append('<div class="animate-bg"><div class="light"></div><div class="lottery-animate-bg"><div class="lotteryuserhead"><img src="' + personObj[0].imgurl + '"/></div><div class="level">恭喜<span class="user-name">' + personObj[0].username + '</span>获得<p class="awards-name">' + showLevel + '</p></div><div class="confirm-btn"><button class="btn btn-lg btn-success fl" onclick="confirmPrize(\'' + personObj[0].winnerclass + '\')"><span id="confirmWin">确定</span></button></div></div>');
     }
     // setTimeout(function () {
     //     $(".animate-bg").animate({"opacity": "0"}, "slow", function () {
@@ -317,43 +329,53 @@ function showLuckAnimate(showLevel, personObj) {
     // if(luckNum > 1){
     //     confirmWin
     //     var count = 3;
-    //     countDown(count, zhongjiangclass)
+    //     countDown(count, winnerclass)
     // }
 
 }
 
-function countDown(val, zhongjiangclass) {
-    if(val == 0) {
-        confirmPrize(zhongjiangclass);
+function countDown(val, winnerclass) {
+    if (val == 0) {
+        confirmPrize(winnerclass);
         return true;
     } else {
-        $('#confirmWin').text('自动抽('+ val +'s)');
+        $('#confirmWin').text('自动抽(' + val + 's)');
         val--;
     }
-    timer = setTimeout(function() {
-        countDown(val, zhongjiangclass)
+    timer = setTimeout(function () {
+        countDown(val, winnerclass)
     }, 1000)
 }
 
 // 中奖翻牌HTML
-function getHTML(number, classname) {
-    console.log(userList[(parseInt(number) % totalperson)]);
+function getHTML(user, classname) {
     var html = '<div class="select-number ' + classname + '">\
                 <div class="block">\
-                    <img src="./img/items/image' + ( '0000' + ((userList[(number % totalperson)] && userList[(number % totalperson)].index)) || 1).slice(-3) + '.png" class="img-circle">\
-                    <span>' + userList[(parseInt(number) % totalperson)].name + '</span>\
+                    <img src="./img/avatar/' + (user.avatar || ('image' + ('0000' + user.gonghao.trim()).slice(-3) + '.png')) + '" class="img-circle">\
+                    <span>' + user.name + '</span>\
 				</div>\
 		</div>';
     $('#luck-list').append(html);
     // container.append(html);
 }
 
-function confirmPrize(zhongjiangclass) {
+function rotationBall(timeout = 1000) {
+    setTimeout(function () {
+        if (timer) {
+            clearInterval(timer);
+        }
+        // 球恢复运行
+        setRotationY(0);
+        timer = setInterval("autoRotate()", 100);
+    }, timeout);
+}
 
-    var zjclass = zhongjiangclass.split(',');
+function confirmPrize(winnerclass) {
+
+    var zjclass = winnerclass.split(',');
 
     // 中奖动画移除
-    $(".animate-bg").animate({"opacity": "0"}, "slow", function () {
+    $(".animate-bg").animate({ "opacity": "0" }, "slow", function () {
         $(".animate-bg").remove();
     });
 
@@ -376,17 +398,8 @@ function confirmPrize(zhongjiangclass) {
                 $('div.' + value).addClass('list');
             }, 1000);
 
-            // 球放大并恢复运转
-            setTimeout(function () {
-                out.css({
-                    animation: 'choujiangScale3 2s forwards'
-                });
-
-                // 球恢复运行
-                $('#ball').css({
-                    animation: 'myball 50s linear infinite'
-                });
-            }, 1000);
+            // 球放恢复运转
+            rotationBall();
 
             // 运动完成自动继续抽奖
             $('div.' + value).one("animationend", function () {
@@ -397,13 +410,6 @@ function confirmPrize(zhongjiangclass) {
                 $('button.btn-success').text('开始抽奖');
                 $('button.btn-success').removeAttr("disabled");
             });
-
-
-
-            // 清除动画
-            // out.css({
-            // 	animation: 'none',
-            // });
 
             // 是否显示获奖标题
             showJiangText(levelType);
